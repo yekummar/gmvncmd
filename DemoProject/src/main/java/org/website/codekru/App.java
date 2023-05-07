dade0d06b6e07313df854e38610996072dcf7a60
yekummar <yekummar@gmail.com>
2023-05-07 20:49:37 +0530

    first mvncmd commit.

---
+++ DemoProject/src/main/java/org/website/codekru/App.java
@@ -0,0 +1,13 @@
+package org.website.codekru;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
